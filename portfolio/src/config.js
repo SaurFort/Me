2068cfc464bf8d2a03a6e33fb0d7e0ca68cc0b01
SaurFort <tou.nathan@gmail.com>
2024-11-01 22:29:20 +0100

    Migrate porfolio from react to sveltekit

--- portfolio/src/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    API: 'http://localhost/SaurFort/portfolio/backend/src/api/',
-    COMMON_STRG: 'http://download.saurfort.fr/',
-    DEFAULT_LANGUAGE: 'en',
-};
-  
-export default config;
